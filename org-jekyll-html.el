a6736df4c48159b5b1784861d0b47957ad9fbd44
Rafael Villarroel <rvf0068@gmail.com>
2017-12-06 13:26:25 -0600

    Fix spacing

--- org-jekyll-html.el
+++ org-jekyll-html.el
@@ -1,15 +1,10 @@
 (org-export-define-derived-backend 'jekyll-html 'html
-  :translate-alist '(
-		     (latex-environment . org-jekyll-html-latex-environment)
+  :translate-alist '((latex-environment . org-jekyll-html-latex-environment)
 		     ;; (latex-fragment . org-jekyll-latex-html-fragment)
 		     (src-block . org-jekyll-html-src-block)
-		     (inner-template . org-jekyll-html-template)
-		     )
-  :options-alist '(
-		   (:published "PUBLISHED" nil "true")
-		   (:layout "LAYOUT" nil "post")
-		   )
-)
+		     (inner-template . org-jekyll-html-template))
+  :options-alist '((:published "PUBLISHED" nil "true")
+		   (:layout "LAYOUT" nil "post")))
 
 (defun org-jekyll-html-src-block (src-block contents info)
   "Transcode SRC-BLOCK element into jekyll format.
@@ -17,39 +12,28 @@ CONTENTS is nil.  INFO is a plist used as a communication
 channel."
   (let ((lang (org-element-property :language src-block))
 	(value (org-element-property :value src-block))
-	(name (or (org-element-property :name src-block) ""))
-	)
+	(name (or (org-element-property :name src-block) "")))
     (if (string= lang "sage")
         (format
          "<div class=\"sage\"><script type=\"text/x-sage\">\n%s</script></div>\n"
          value)
-      ;; (format
-      ;;  "``` %s %s\n%s```"
-      ;;  lang name value)
       (format
        "{%% highlight %s %%}\n %s{%% endhighlight %%}"
-       lang value)
-      )))
+       lang value))))
 
 (defun org-jekyll-html-latex-environment (latex-environment contents info)
-  (let* (
-	 (value (org-remove-indentation
+  (let* ((value (org-remove-indentation
 		 (org-element-property :value latex-environment)))
 	 (replaced (replace-regexp-in-string
-		    "\\\\begin{displaymath}\\|\\\\end{displaymath}" "$$" value))
-	 )
-    replaced
-    ))
+		    "\\\\begin{displaymath}\\|\\\\end{displaymath}" "$$" value)))
+    replaced))
 
 (defun org-jekyll-html-latex-fragment (latex-fragment contents info)
-  (let* (
-	 (latex (org-element-property :value latex-fragment))
+  (let* ((latex (org-element-property :value latex-fragment))
 	 (inline-latex (replace-regexp-in-string "\\\\(\\|\\\\)" "$$" latex))
-	 (bars-removed (replace-regexp-in-string "|" "\\\\vert " inline-latex))
-	 )
+	 (bars-removed (replace-regexp-in-string "|" "\\\\vert " inline-latex)))
     ;; (message "Latex-fragment is %s" latex)
-    bars-removed
-    ))
+    bars-removed))
 
 ;; We have to transform
 ;; <img src="recta.png" alt="recta" width="400" align="center" />
@@ -101,12 +85,10 @@ channel."
 		 "\n\n    -"
 		 "
     -"
-		 ignoreh
-		 ))
+		 ignoreh))
 	 (frontmatter
 	  "---\nlayout: %s\ntitle: %s\ndate: %s %s\ncomments: true\npublished: %s\ncategories: %s\n---\n\n"))
-    (concat (format frontmatter layout title date time published keywords) lists)
-    ))
+    (concat (format frontmatter layout title date time published keywords) lists)))
 
 (defun org-jekyll-html-export-as-jekyll
   (&optional async subtreep visible-only body-only ext-plist)
